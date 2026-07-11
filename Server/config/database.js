8617711ce9d5cbda3c987d8b89f2aad8b587005f
sagarpandey <sagarwebsec@gmail.com>
2026-07-11 17:24:02 +0530

    some issue was fixed :maintainence

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
-const dns = require("dns");
 require("dotenv").config();
 
-// Fix: Reliance/Jio ISP DNS cannot resolve MongoDB Atlas SRV records in Node.js.
-// Override to use Google Public DNS instead.
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 exports.connect = () => {
     mongoose.connect(process.env.MONGODB_URL, {
